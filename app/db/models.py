7990bb6ece78d57107cbb34ff115e3b58ef09ed9
Vinish M <vinishuchiha@gmail.com>
2025-01-08 21:28:37 +0530

    db read all endpoint added

--- app/db/models.py
+++ app/db/models.py
@@ -1,4 +1,4 @@
-from database import Base
+from app.db.database import Base
 from sqlalchemy import Column, Integer, String, Boolean
 
 class Books(Base):
--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Depends
 from app.config import Settings, get_settings
-from app.db.database import engine
+from app.db.database import engine, get_db
 from app.db import models
+from typing import Annotated
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
@@ -14,3 +16,7 @@ def hello(settings: Settings = Depends(get_settings)):
         "environment": settings.environment,
         "testing": settings.testing
         }
+
+@app.get('/')
+async def read_all(db: Annotated[Session, Depends(get_db)]):
+    return db.query(models.Books).all()
