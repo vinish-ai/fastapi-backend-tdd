14de4a390b47ea57e0bd2b0ad705a5f0b803360e
Vinish M <vinishuchiha@gmail.com>
2025-01-09 07:51:48 +0530

    get_book endpoint added

--- app/main.py
+++ app/main.py
@@ -1,13 +1,16 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
 from app.config import Settings, get_settings
 from app.db.database import engine, get_db
 from app.db import models
 from typing import Annotated
 from sqlalchemy.orm import Session
+from starlette import status
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+db_dependency = Annotated[Session, Depends(get_db)]
 
 @app.get("/ping")
 def hello(settings: Settings = Depends(get_settings)):
@@ -17,6 +20,13 @@ def hello(settings: Settings = Depends(get_settings)):
         "testing": settings.testing
         }
 
-@app.get('/')
-async def read_all(db: Annotated[Session, Depends(get_db)]):
+@app.get('/book/all', status_code=status.HTTP_200_OK)
+async def read_all(db: db_dependency):
     return db.query(models.Books).all()
+
+@app.get('/book/{book_id}', status_code=status.HTTP_200_OK)
+async def get_book(db: db_dependency, book_id: int = Path(gt=0)):
+    book = db.query(models.Books).filter(models.Books.id == book_id).first()
+    if book is not None:
+        return book
+    raise HTTPException(status_code=404, detail='Book Not Found')
