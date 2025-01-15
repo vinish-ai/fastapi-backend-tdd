d80be3741d3a52e2fe3e46799cca4ce4877ba127
Vinish M <vinishuchiha@gmail.com>
2025-01-15 22:55:15 +0530

    admin and user router added in main

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.db.database import engine
 from app.db import models
-from app.routers import books, auth
+from app.routers import books, auth, admin, users
 
 app = FastAPI()
 
@@ -9,3 +9,5 @@ models.Base.metadata.create_all(bind=engine)
 
 app.include_router(books.router)
 app.include_router(auth.router)
+app.include_router(admin.router)
+app.include_router(users.router)
